7a208a19480adf6260f29818d24e11210f515a9a
Hector Corpus <hectorcorpus121505@gmail.com>
2023-09-20 15:56:32 -0500

    git init

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,154 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+
+
+//import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private Spark leftMotor0 = new Spark(0);
+  private Spark leftMotor1 = new Spark(1);
+  private Spark rightMotor2 = new Spark(2);
+  private Spark rightMotor3 = new Spark(3);
+  
+  private Joystick controller = new Joystick(0);
+  private SlewRateLimiter filter = new SlewRateLimiter(0.5);
+
+  MotorControllerGroup left = new MotorControllerGroup(leftMotor1, leftMotor0);
+  MotorControllerGroup right = new MotorControllerGroup(rightMotor3, rightMotor2);
+
+  private DifferentialDrive drive = new DifferentialDrive(left, right);  
+  private final Timer timer = new Timer();
+
+  /**
+   * 
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+  @Override
+  public void robotInit() {
+    right.setInverted(true);
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+  }
+
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {}
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+    timer.reset();
+  }
+
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        leftMotor1.set(0.8);
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        leftMotor0.set(0.8);
+        break;
+    }
+  }
+
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {}
+
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {
+    //double speed = 1;
+    double speed = -controller.getRawAxis(1) * 1;
+    double turn = -controller.getRawAxis(4) * 0.7;
+    drive.arcadeDrive(deadBand(speed,0.05),turn);
+  }
+
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
+
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
+
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
+
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
+
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
+
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+
+  /**This is a deadband for the joystick controller, basically it just makes it
+  *   so you have to move the joystick a certian amount to register input.
+  */
+  public static double deadBand(double value, double deadband){
+    if(Math.abs(value) > deadband){
+      if(value > 0.0){
+        return (value - deadband) / (1.0 - deadband);
+      } else{
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else{
+      return 0.0;
+    }
+  }
+}
